0392c6d37f56eb9e7dda92425ed39bef99b15313
Nikunj Das Kasat <nikunjdaskasat@gmail.com>
2020-04-17 21:22:55 +0530

    Completed v3

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    User = require("./models/user"),
+    seedDB = require("./seeds"),
     port = 3000;
+
+seedDB();
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
-
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get('/', function (req, res) {
     res.render("landing");
@@ -36,26 +34,26 @@ app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = { name: name, image: image, description: description};
-    Campground.create(newCampground, function(err, newlyCreated) {
-        if(err) {
+    var newCampground = { name: name, image: image, description: description };
+    Campground.create(newCampground, function (err, newlyCreated) {
+        if (err) {
             console.log(err);
         } else {
             res.redirect("/campgrounds");
         }
-    });    
+    });
 });
 
 app.get("/campgrounds/new", function (req, res) {
     res.render("new");
 });
 
-app.get("/campgrounds/:id", function(req, res){
-    Campground.findById(req.params.id, function(err, foundCampground){
-        if(err) {
+app.get("/campgrounds/:id", function (req, res) {
+    Campground.findById(req.params.id).populate("comments").exec(function (err, foundCampground) {
+        if (err) {
             console.log(err);
         } else {
-            res.render("show", {campground: foundCampground});
+            res.render("show", { campground: foundCampground });
         }
     });
 });

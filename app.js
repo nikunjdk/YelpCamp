95390ea366acabe91ff5064cd4e2f391b2ed6749
Nikunj Das Kasat <nikunjdaskasat@gmail.com>
2020-04-21 21:16:27 +0530

    Completed v6

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
     Campground = require("./models/campground"),
     Comment = require("./models/comment"),
     User = require("./models/user"),
@@ -14,9 +16,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "/public"));
 
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+})
+
 app.set("view engine", "ejs");
 
 seedDB();
+
+app.use(require('express-session')({
+    secret: "This is a secret...",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function (req, res) {
     res.render("landing");
@@ -60,7 +79,7 @@ app.get("/campgrounds/:id", function (req, res) {
     });
 });
 
-app.get("/campgrounds/:id/comments/new", function (req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function (req, res) {
     Campground.findById(req.params.id, function (err, campground) {
         if (err) {
             console.log(err);
@@ -70,7 +89,7 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
     });
 });
 
-app.post("/campgrounds/:id/comments", function (req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function (req, res) {
     Campground.findById(req.params.id, function (err, campground) {
         if (err) {
             console.log(err);
@@ -87,6 +106,46 @@ app.post("/campgrounds/:id/comments", function (req, res) {
         }
     });
 });
+
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+
+app.post('/register', function (req, res) {
+    var newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.send('register');
+        }
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/campgrounds');
+        });
+    });
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/campgrounds',
+    failureRedirect: '/login'
+}), function (req, res) {
+
+});
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/campgrounds');
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 app.listen(port, function () {
     console.log("Server has started");
